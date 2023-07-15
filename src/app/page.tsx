ab458e9951ab21ecaee312246619a54f6f86d105
josh warren <81135679+joshwrn@users.noreply.github.com>
2023-07-15 04:29:53 -0700

    can draw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 
 import React, { useCallback, useEffect, useState } from 'react'
-import { PointerLockControls, Reflector, Sky, Html } from '@react-three/drei'
-import { Canvas, useFrame } from '@react-three/fiber'
+import {
+  PointerLockControls,
+  Reflector,
+  Sky,
+  Html,
+  Environment,
+} from '@react-three/drei'
+import { Canvas, ThreeEvent, useFrame } from '@react-three/fiber'
 import styled from 'styled-components'
 
 const CanvasContainer = styled.main`
@@ -14,26 +20,43 @@ const CanvasContainer = styled.main`
 const Drawing = styled.div`
   border: 1px solid black;
   color: red;
+  position: absolute;
+  z-index: 100;
+  pointer-events: none;
   canvas {
     border: 1px solid black;
-    width: 1000px;
-    height: 500px;
+    width: 300px;
+    height: 150px;
   }
 `
 
-const DrawingCanvas = () => {
-  const [domNode, setDomNode] = useState<HTMLCanvasElement | null>(null)
-  const onRefChange = useCallback((node: HTMLCanvasElement) => {
-    setDomNode(node)
-  }, [])
+const canvasResolution = {
+  width: 3000,
+  height: 1500,
+}
+
+const DrawingCanvas = ({ domNode }: { domNode: HTMLCanvasElement | null }) => {
   const textureRef = React.useRef<THREE.CanvasTexture>(null)
+  const [pos, setPos] = useState<{
+    x: number
+    y: number
+    z: number
+  }>({
+    x: 0,
+    y: 0,
+    z: 0,
+  })
 
   useEffect(() => {
     if (domNode) {
       const ctx = domNode.getContext(`2d`)
       if (ctx) {
+        domNode.width = canvasResolution.width
+        domNode.height = canvasResolution.height
+        ctx.fillStyle = `white`
+        ctx.fillRect(0, 0, canvasResolution.width, canvasResolution.height)
         ctx.fillStyle = `rgb(200, 0, 0)`
-        ctx.fillRect(10, 10, 50, 50)
+        ctx.fillRect(0, 0, 50, 50)
       }
     }
   }, [domNode])
@@ -43,41 +66,75 @@ const DrawingCanvas = () => {
       textureRef.current.needsUpdate = true
     }
   })
+
+  const draw = (e: ThreeEvent<PointerEvent>) => {
+    if (domNode) {
+      const ctx = domNode.getContext(`2d`)
+      if (ctx) {
+        const position = {
+          x: e.point.x,
+          y: e.point.y,
+          z: e.point.z,
+        }
+        setPos(position)
+        const x2d = e.point.x * 10
+        const y2d = e.point.y * 10
+        const coord = {
+          x: canvasResolution.width - x2d,
+          y: canvasResolution.height - y2d,
+        }
+
+        console.log(e.distance, e.point.x, e.point.y, e.point.z)
+        ctx.beginPath()
+        ctx.lineWidth = 5
+        ctx.lineCap = `round`
+        ctx.strokeStyle = `#ee00ff`
+        ctx.moveTo(coord.x, coord.y)
+        // reposition(event)
+        ctx.lineTo(coord.x, coord.y)
+        ctx.stroke()
+      }
+    }
+  }
+
   return (
     <>
-      <mesh scale={[100, 30, 1]} position={[0, 0, 50]} receiveShadow castShadow>
-        <boxGeometry />
-        {/* <meshStandardMaterial
-      color="rgb(255, 255, 255)"
-      metalness={0.3}
-      roughness={0.5}
-      attach="material"
-      envMapIntensity={0.2}
-    /> */}
-
-        <meshBasicMaterial>
-          <canvasTexture attach="map" ref={textureRef} image={domNode} />
-        </meshBasicMaterial>
-      </mesh>
-      <Html
-        as="div" // Wrapping element (default: 'div')
-        rotation={[0, 0, 0]}
-        position={[0, 0, 35]}
-        occlude
+      {/* <mesh position={[pos.x, pos.y, pos.z]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="red" />
+      </mesh> */}
+      <mesh
+        scale={[canvasResolution.width / 10, canvasResolution.height / 10, 1]}
+        onPointerMove={draw}
+        position={[150, 75, 40]}
+        receiveShadow
+        castShadow
       >
-        <Drawing>
-          hello
-          <canvas ref={onRefChange} />
-        </Drawing>
-      </Html>
+        <boxGeometry />
+        <meshStandardMaterial metalness={0.2} roughness={0.1}>
+          <canvasTexture attach="map" ref={textureRef} image={domNode} />
+        </meshStandardMaterial>
+      </mesh>
     </>
   )
 }
 
 export default function Home() {
+  const [domNode, setDomNode] = useState<HTMLCanvasElement | null>(null)
+  const onRefChange = useCallback((node: HTMLCanvasElement) => {
+    setDomNode(node)
+  }, [])
   return (
     <CanvasContainer>
-      <Canvas shadows gl={{ alpha: false }} camera={{ fov: 80 }}>
+      <Drawing>
+        <canvas ref={onRefChange} />
+      </Drawing>
+      <Canvas
+        shadows
+        gl={{ alpha: false }}
+        camera={{ fov: 80, position: [50, 15, 0] }}
+      >
+        <Environment preset="park" />
         <color attach="background" args={[`white`]} />
         <PointerLockControls />
         <Sky
@@ -86,7 +143,7 @@ export default function Home() {
           inclination={0}
           azimuth={0.25}
         />
-        <DrawingCanvas />
+        <DrawingCanvas domNode={domNode} />
         <ambientLight intensity={0.8} />
         <pointLight
           color="white"
@@ -94,7 +151,7 @@ export default function Home() {
           position={[0, 10, 50]}
           intensity={2}
         />
-        <mesh rotation={[-3.14 / 2, 0, 0]} position={[0, -15, 0]} receiveShadow>
+        <mesh rotation={[-3.14 / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
           <planeGeometry args={[300, 300]} />
           <meshStandardMaterial
             color="rgb(255, 255, 255)"
