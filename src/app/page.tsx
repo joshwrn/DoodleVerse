64192783c10904a5e5dd6a1225a670cfa6dcec5d
josh warren <81135679+joshwrn@users.noreply.github.com>
2023-07-15 00:33:40 -0700

    add canvas texture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { PointerLockControls, Reflector, Sky } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import React, { useCallback, useEffect, useState } from 'react'
+import { PointerLockControls, Reflector, Sky, Html } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
 import styled from 'styled-components'
 
 const CanvasContainer = styled.main`
@@ -9,6 +10,69 @@ const CanvasContainer = styled.main`
   height: 100vh;
   display: flex;
 `
+
+const Drawing = styled.div`
+  border: 1px solid black;
+  color: red;
+  canvas {
+    border: 1px solid black;
+    width: 1000px;
+    height: 500px;
+  }
+`
+
+const DrawingCanvas = () => {
+  const [domNode, setDomNode] = useState<HTMLCanvasElement | null>(null)
+  const onRefChange = useCallback((node: HTMLCanvasElement) => {
+    setDomNode(node)
+  }, [])
+  const textureRef = React.useRef<THREE.CanvasTexture>(null)
+
+  useEffect(() => {
+    if (domNode) {
+      const ctx = domNode.getContext(`2d`)
+      if (ctx) {
+        ctx.fillStyle = `rgb(200, 0, 0)`
+        ctx.fillRect(10, 10, 50, 50)
+      }
+    }
+  }, [domNode])
+
+  useFrame(() => {
+    if (textureRef.current) {
+      textureRef.current.needsUpdate = true
+    }
+  })
+  return (
+    <>
+      <mesh scale={[100, 30, 1]} position={[0, 0, 50]} receiveShadow castShadow>
+        <boxGeometry />
+        {/* <meshStandardMaterial
+      color="rgb(255, 255, 255)"
+      metalness={0.3}
+      roughness={0.5}
+      attach="material"
+      envMapIntensity={0.2}
+    /> */}
+
+        <meshBasicMaterial>
+          <canvasTexture attach="map" ref={textureRef} image={domNode} />
+        </meshBasicMaterial>
+      </mesh>
+      <Html
+        as="div" // Wrapping element (default: 'div')
+        rotation={[0, 0, 0]}
+        position={[0, 0, 35]}
+        occlude
+      >
+        <Drawing>
+          hello
+          <canvas ref={onRefChange} />
+        </Drawing>
+      </Html>
+    </>
+  )
+}
 
 export default function Home() {
   return (
@@ -22,21 +86,7 @@ export default function Home() {
           inclination={0}
           azimuth={0.25}
         />
-        <mesh
-          scale={[100, 30, 1]}
-          position={[0, 0, 50]}
-          receiveShadow
-          castShadow
-        >
-          <boxGeometry />
-          <meshStandardMaterial
-            color="rgb(255, 255, 255)"
-            metalness={0.3}
-            roughness={0.5}
-            attach="material"
-            envMapIntensity={0.2}
-          />
-        </mesh>
+        <DrawingCanvas />
         <ambientLight intensity={0.8} />
         <pointLight
           color="white"
