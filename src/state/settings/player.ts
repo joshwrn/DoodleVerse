f599071a86e879c2b7ed8a4437eb19a452772d37
josh warren <81135679+joshwrn@users.noreply.github.com>
2023-07-27 19:04:31 -0700

    add other player state

--- src/state/settings/player.ts
+++ src/state/settings/player.ts
@@ -1,10 +1,26 @@
 import { create } from 'zustand'
 import { nanoid } from 'nanoid'
 
+export type Player = {
+  userId: string
+  brushColor: string
+  avatar: 'man' | 'woman'
+  position: {
+    x: number
+    y: number
+    z: number
+  }
+  rotationY: number
+}
+
 export const usePlayerStore = create<{
   userId: string
   setUserId: (userId: string) => void
+  otherPlayers: Player[]
+  setOtherPlayers: (otherPlayers: Player[]) => void
 }>((set) => ({
   userId: nanoid(),
   setUserId: (userId) => set({ userId }),
+  otherPlayers: [],
+  setOtherPlayers: (otherPlayers) => set({ otherPlayers }),
 }))
